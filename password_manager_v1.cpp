309761dacab76709bd95633723230592ba6c175b
fireninj-p <84732893+fireninj-p@users.noreply.github.com>
2023-04-11 10:01:28 -0700

    Update password_manager_v1.cpp

--- password_manager_v1.cpp
+++ password_manager_v1.cpp
@@ -2,40 +2,31 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-//#include <bits/stdc++.h>
 #include <cstring>
 #include <list>
-#include <filesystem>
 #include <cstdio>
-//#define MAX 1000;
-//header files
-//to do:
-//fix loop error where program doesn't loop back to main menu
-//after signing in and choosing to go back to the menu
 
-//symbols are not recommended in Master Passwords
+
 using namespace std;
 
 int main()
 {
-
-    int proceed = 0;
+    int proceed = 0, proceed_case = 0;
     int initial_input;
-    int proceed_case_1 = 0;
-    int proceed_case_2 = 0;
-    int proceed_case_3 = 0;
-    int proceed_case_4 = 0;
 
     do
     {
+        proceed = 0;
+        proceed_case = 0;
+
         user_welcome();
         cout << "Enter Here: ";
         cin >> initial_input;
-        switch (initial_input)
+        switch(initial_input)
         {
             case 1:
-                do
-                {
+                do{
+                    proceed_case = 0;
                     sign_in();
                     cout << endl;
                     cout <<" Would you like to sign in again?\n";
@@ -43,27 +34,29 @@ int main()
                     cout << "Enter 2 to enter the main menu  \n";
                     cout << "Enter 3 to quit the program \n";
                     cout << "Enter here: ";
-                    cin >> proceed_case_1;
-                }while(proceed_case_1 == 1);
-                if (proceed_case_1 == 2)
-                {proceed == 1;}
+                    cin >> proceed_case;
+                    if (proceed_case == 2)
+                    {
+                        proceed = 1;
+                    }
+                }while(proceed_case == 1);
             break;
-            
+
             case 2:
-                do
-                {
+                do{
+                    proceed_case = 0;
                     create_account();
-                    cout << endl;
                     cout <<" Would you like to create another account?\n";
                     cout <<" Enter 1 to create another account,       \n";
                     cout << "Enter 2 to enter the main menu  \n";
                     cout << "Enter 3 to quit the program \n";
                     cout << "Enter here: ";
-                    cin >> proceed_case_2;
-
-                }while(proceed_case_2 == 1);
-                if (proceed_case_2 == 2)
-                {proceed = 1;}
+                    cin >> proceed_case;
+                    if (proceed_case == 2)
+                    {
+                        proceed = 1;
+                    }
+                }while(proceed_case == 1);
             break;
 
             case 3:
@@ -76,6 +69,5 @@ int main()
                 cout << "Whatever you typed in is not accepted by the program\n";
                 cout << "----------------------------------------------------\n";
         }
-    }
-    while (proceed == 1);
+    }while(proceed == 1);
 }
